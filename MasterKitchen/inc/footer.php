477f431ea6078dff70ddd3528db50758395feac1
Dimitris Pertsinidis <76684058+dpertsin@users.noreply.github.com>
2021-06-18 21:17:43 +0300

    Add files via upload

---
+++ MasterKitchen/inc/footer.php
@@ -0,0 +1,6 @@
+<footer class="pull-left footer">
+			<p class="col-md-12">
+				<hr class="divider">
+				Copyright &COPY; 2021 <a href="">MasterKitchen</a>
+			</p>
+</footer>
---
+++ MasterKitchen/lib/Session.php
@@ -0,0 +1,59 @@
+<?php 
+
+class Session{
+
+	public static function init(){
+		
+
+		if (version_compare(phpversion(),'5.4.0', '<')) {
+			if(session_id() ==''){
+			session_start();
+		}
+
+		}
+
+	if (session_status() == PHP_SESSION_NONE) {
+			    session_start();
+			}	
+		
+			@session_start();
+	}
+
+	public static function set($key, $val){
+		$_SESSION[$key] = $val;
+	}
+	public static function get($key){
+		if(isset($_SESSION[$key])){
+			return $_SESSION[$key];
+		} else {
+			return false;
+		}
+	}
+
+	public function checkSession()
+	{
+		if(self::get("login")==false) {
+			self::destroy();
+			header("Location: login.php");
+		}
+
+	}
+
+	public function checkLogin()
+	{
+		if(self::get("login")==true) {
+			self::destroy();
+			header("Location: panel.php");
+		}
+	}
+
+	public function destroy()
+	{
+		session_destroy();
+		session_unset();
+		header("Location: login.php");
+	}
+}
+
+
+ ?>
